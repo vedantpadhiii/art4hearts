b681230152402dd2827dcebf29244eb5b75291ea
vpadhi <vpadhi08@gmail.com>
2025-11-17 21:40:56 -0800

    Remove DSCF5420.jpg from gallery

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1432,7 +1432,6 @@ const Home: React.FC = () => {
           >
             <GalleryCarousel 
               images={[
-                '/gallery/DSCF5420.jpg',
                 '/gallery/DSCF5468.jpg',
                 '/gallery/IMG_8906.jpg',
                 '/gallery/club_rush_sept_5.jpg',
