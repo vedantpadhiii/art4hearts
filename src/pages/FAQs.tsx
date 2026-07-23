9bb19605ee5171e42f50ec93b0fdb56adf196094
vpadhi <vpadhi08@gmail.com>
2026-07-23 12:40:17 -0700

    faq fix

--- src/pages/FAQs.tsx
+++ src/pages/FAQs.tsx
@@ -26,7 +26,7 @@ const braceletsAndKitsFAQs = [
   },
   {
     question: "How can I donate the bracelets and art therapy kits?",
-    answer: "You can:\n. 1. Ship ART THERAPY KITS to our P.O. Box address found in the service request forms (we do not accept bracelets in our P.O.)\n2. Donate it to your local charitable organization (this includes both bracelets and art therapy kits)."
+    answer: "You can:\n1. Ship ART THERAPY KITS to our P.O. Box address found in the service request forms (we do not accept bracelets in our P.O.)\n2. Donate it to your local charitable organization (this includes both bracelets and art therapy kits)."
   },
   {
     question: "Do we always have to pay for the supplies and shipping?",
